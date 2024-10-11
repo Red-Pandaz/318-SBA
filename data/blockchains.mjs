1b0833d3db31fea6d92227c6cc56a6f7fcf4088d
Red Panda <Deep_Sea_Penguin@protonmail.com>
2024-10-11 16:10:42 -0400

    create sample data

---
+++ data/blockchains.mjs
@@ -0,0 +1,69 @@
+export const blockchains = [
+    {
+        "id": 1,
+        "name": "Bitcoin",
+        "currencies": [1]
+    },
+    {
+        "id": 2,
+        "name": "Ethereum",
+        "currencies": [2, 3, 7, 15, 16, 18, 19, 20]
+    },
+    {
+        "id": 3,
+        "name": "Binance Chain",
+        "ticker": "BNB",
+        "currencies": [3]
+    },
+    {
+        "id": 4,
+        "name": "Solana",
+        "ticker": "SOL",
+        "currencies": [5]
+    },
+    {
+        "id": 5,
+        "name": "Ripple",
+        "currencies": [6]
+    },
+    {
+        "id": 6,
+        "name": "Cardano",
+        "currencies": [8]
+    },
+    {
+        "id": 7,
+        "name": "Dogecoin",
+        "currencies": [9]
+    },
+    {
+        "id": 8,
+        "name": "Polygon",
+        "currencies": [10]
+    },
+    {
+        "id": 9,
+        "name": "Toncoin",
+        "currencies": [11]
+    },
+    {
+        "id": 10,
+        "name": "Tron",
+        "currencies": [12]
+    },
+    {
+        "id": 11,
+        "name": "Litecoin",
+        "currencies": [13]
+    },
+    {
+        "id": 12,
+        "name": "Polkadot",
+        "currencies": [14]
+    },
+    {
+        "id": 13,
+        "name": "Avalanche",
+        "currencies": [17]
+    }
+]
---
+++ data/coins.mjs
@@ -0,0 +1,122 @@
+export const coins = [
+    {
+        "id": 1,
+        "name": "Bitcoin",
+        "ticker": "BTC",
+        "blockchain": 1
+    },
+    {
+        "id": 2,
+        "name": "Ethereum",
+        "ticker": "ETH",
+        "blockchain": 2
+    },
+    {
+        "id": 3,
+        "name": "Tether",
+        "ticker": "USDT",
+        "blockchain": 2
+    },
+    {
+        "id": 4,
+        "name": "Binance Coin",
+        "ticker": "BNB",
+        "blockchain": 3
+    },
+    {
+        "id": 5,
+        "name": "Solana",
+        "ticker": "SOL",
+        "blockchain": 4
+    },
+    {
+        "id": 6,
+        "name": "Ripple",
+        "ticker": "XRP",
+        "blockchain": 5
+    },
+    {
+        "id": 7,
+        "name": "USD Coin",
+        "ticker": "USDC",
+        "blockchain": 2
+    },
+    {
+        "id": 8,
+        "name": "Cardano",
+        "ticker": "ADA",
+        "blockchain": 6
+    },
+    {
+        "id": 9,
+        "name": "Dogecoin",
+        "ticker": "DOGE",
+        "blockchain": 7
+    },
+    {
+        "id": 10,
+        "name": "Polygon",
+        "ticker": "MATIC",
+        "blockchain": 8
+    },
+    {
+        "id": 11,
+        "name": "Toncoin",
+        "ticker": "TON",
+        "blockchain": 9
+    },
+    {
+        "id": 12,
+        "name": "Tron",
+        "ticker": "TRX",
+        "blockchain": 10
+    },
+    {
+        "id": 13,
+        "name": "Litecoin",
+        "ticker": "LTC",
+        "blockchain": 11
+    },
+    {
+        "id": 14,
+        "name": "Polkadot",
+        "ticker": "DOT",
+        "blockchain": 12
+    },
+    {
+        "id": 15,
+        "name": "Shiba Inu",
+        "ticker": "SHIB",
+        "blockchain": 2
+    },
+    {
+        "id": 16,
+        "name": "Dai",
+        "ticker": "Dai",
+        "blockchain": 2
+    },
+    {
+        "id": 17,
+        "name": "Avalanche",
+        "ticker": "AVAX",
+        "blockchain": 13
+    },
+    {
+        "id": 18,
+        "name": "Wrapped Bitcoin",
+        "ticker": "WBTC",
+        "blockchain": 2
+    },
+    {
+        "id": 19,
+        "name": "Chainlink",
+        "ticker": "LINK",
+        "blockchain": 2
+    },
+    {
+        "id": 20,
+        "name": "Uniswap",
+        "ticker": "UNI",
+        "blockchain": 2
+    }
+]
